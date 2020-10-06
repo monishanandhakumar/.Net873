7c1fbe37f5585b0b31c1d126c530b9c2f3481f3c
monishanandhakumar <mmonishadevi.cse@gmail.com>
2020-10-06 18:04:42 +0530

    Csharp Day4

---
+++ CsharpDay4/PrjCsharpday4/Inheritance/AbstractionEg.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+   abstract class RBI
+    {
+        int empno = 20;
+       public  abstract int Homeloan();
+        public abstract int Educationloan();
+
+        //Non abstract method
+        internal void RBIEmployee()
+        {
+            Console.WriteLine("RBI Employee details:{0}",empno);
+        }
+    }
+    class SBI:RBI
+    {
+        public override int Homeloan()
+        {
+            return 9;
+        }
+       public override int Educationloan()
+        {
+            return 10;
+        }
+    }
+    class HDFC : RBI
+    {
+        public override int Homeloan()
+        {
+            return 10;
+        }
+        public override int Educationloan()
+        {
+            return 11;
+        }
+    }
+    class AbstractionEg
+    {
+        static void Main()
+        {
+            //cant create object for abstract class
+            // RBI r = new RBI();
+            //reference for rbi class
+
+            // SBI sbi = new SBI();
+            /* sbi.RBIEmployee();
+            Console.WriteLine("sbi h loan:{0}",sbi.Homeloan());
+            Console.WriteLine("sbi e loan:{0}", sbi.Educationloan());
+            HDFC hdfc = new HDFC();
+            Console.WriteLine("hdfc h loan:{0}", hdfc.Homeloan());
+            Console.WriteLine("hdfc e loan:{0}", hdfc.Educationloan());*/
+
+            //Run Time Polymorphism
+            RBI r;
+            //object for SBI
+            r = new SBI();
+            r.RBIEmployee();
+            Console.WriteLine("sbi h loan:{0}", r.Homeloan());
+            Console.WriteLine("sbi e loan:{0}", r.Educationloan());
+            r = new HDFC();
+            Console.WriteLine("hdfc h loan:{0}", r.Homeloan());
+            Console.WriteLine("hdfc e loan:{0}", r.Educationloan());
+            Console.Read();
+        }
+    }
+}
---
+++ CsharpDay4/PrjCsharpday4/Inheritance/ProtectedInternalEg.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Parts
+    {
+      protected internal  int wheel;
+        protected internal string Motor;
+
+        protected internal  void DisplayParts()
+        {
+            Console.WriteLine("Wheel:{0}||Motor{1}",wheel,Motor);
+        }
+
+
+    }
+    class ProtectedInternalEg
+    {
+    }
+}
