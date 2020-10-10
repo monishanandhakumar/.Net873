0a2f99a828722eb819b65d4b420f281ad9cb986a
monishanandhakumar <mmonishadevi.cse@gmail.com>
2020-10-10 21:33:53 +0530

    CSHARP DAY 8

---
+++ CsharpDay8/PrjAdo.NetEg/ConnectedArchitectureEg/Crud_ConnectedArchitectureEg.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Data.SqlClient; //1.Adding sql server- data provider
+using System.Data;
+
+
+namespace ConnectedArchitectureEg
+{
+    class Region
+    {
+        public int RegionId { get; set; }
+        public string RegionDescription { get; set; }
+
+        internal void GetRegion()
+        {
+            Console.WriteLine("Enter RegionId");
+            RegionId = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter RegionDescription");
+            RegionDescription = Console.ReadLine();
+        }
+    }
+
+    class DataAccess
+
+    {
+        //2. create   SqlConnection object
+        //connection string 
+        SqlConnection con = null;
+        //Creating object for command class
+        SqlCommand cmd;
+     
+        public SqlConnection GetConnection()
+        {
+            //Windows authentication
+           con = new SqlConnection(
+            "Data Source = DESKTOP-U8J1M3C\\MSSQLSERVER01; Initial Catalog = Northwind; Integrated Security = true");
+          
+            con.Open();
+            return con;
+        }
+        //select 
+        public void DisplayRegion()
+        {
+            try
+            {/* SqlConnection con1;
+             con1 = GetConnection();*/
+             //reuse object
+                con = GetConnection();
+                SqlDataReader dr;
+                // string s = "select * from Region";
+                cmd = new SqlCommand("select * from Region", con);
+                dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    //Console.WriteLine(dr[0] + "  " + dr[1]);
+                    Console.WriteLine(dr["RegionId"] + "  " + dr["RegionDescription"]);
+                }
+
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e); 
+
+            }
+
+            finally
+            {
+                con.Close();
+            }
+           
+        }
+
+        //Execute scalar
+     internal   void GetbyExecuteScalar()
+        {
+            con = GetConnection();
+            cmd = new SqlCommand("select count(RegionID) from Region", con);
+           int count= Convert.ToInt32(cmd.ExecuteScalar()); //execute scalar retuns object
+            Console.WriteLine("No of Region:{0}",count);
+        }
+
+        //Update
+        internal void EditRegion()
+        {
+            Region region = new Region();
+            Console.WriteLine("Please the which region description to be updated");
+            region.GetRegion();
+            con = GetConnection();
+            cmd = new SqlCommand("update Region set RegionDescription=@RDesc where RegionId=@Rid", con);
+          
+            cmd.Parameters.AddWithValue("@Rid", region.RegionId);
+            cmd.Parameters.AddWithValue("@RDesc",region.RegionDescription);
+            int i= cmd.ExecuteNonQuery();
+            Console.WriteLine("Rows Updated are:{0}",i);
+            
+        }
+
+        //StoredProcedure with Parameters
+    internal  void CallProcedure()
+        {
+            con = GetConnection();
+
+            Console.WriteLine("Enter the Customer ID");
+            string custId = Console.ReadLine();
+                                //Procedurename
+            cmd = new SqlCommand("CustOrdersOrders", con);
+                             //using System.Data
+            cmd.CommandType = CommandType.StoredProcedure;
+          cmd.Parameters.AddWithValue("@CustomerID", custId);
+            
+            //Or
+            /*SqlParameter sqlParameter = new SqlParameter("@CustomerID", custId);
+            sqlParameter.Direction = ParameterDirection.Input;
+            sqlParameter.DbType = DbType.String;
+            cmd.Parameters.Add(sqlParameter);*/
+            
+            SqlDataReader rdr;
+        
+            rdr = cmd.ExecuteReader();
+
+            while (rdr.Read())
+            {
+                Console.WriteLine(rdr["OrderId"] + " " + rdr["OrderDate"]+" "+rdr["RequiredDate"]);
+
+            }
+            con.Close();
+        }
+
+    }
+
+
+    class ClientEnd
+    {
+        static void Main()
+        {
+            DataAccess d = new DataAccess();
+            try
+            {
+              /*  d.DisplayRegion();
+                d.GetbyExecuteScalar();
+                d.EditRegion();*/
+                d.CallProcedure();
+                
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+
+            }
+         
+            Console.Read();
+        }
+    }
+
+
+
+    class Crud_ConnectedArchitectureEg
+    {
+
+    }
+}
---
+++ CsharpDay8/PrjAdo.NetEg/TransactionScopeEg/Local_DistributedTransactionEg.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Data.SqlClient; 
+using System.Transactions;
+
+namespace TransactionScopeEg
+{
+    class Local_DistributedTransactionEg
+    {
+        static void LocalTransactionScope()
+        {
+
+          /*  using{
+
+            }*/
+            int i, j;
+            //Transaction class
+            SqlTransaction mytran=null;
+            string myconnectionString = "Data Source = DESKTOP-U8J1M3C\\MSSQLSERVER01; Initial Catalog = Northwind; Integrated Security = true";
+            //transaction
+            using (TransactionScope myscope = new TransactionScope())
+            {
+                //Connection
+                using (SqlConnection conn = new SqlConnection(myconnectionString))
+                {
+                    conn.Open();
+                    try
+                    {
+                        //Begin transaction
+                        mytran = conn.BeginTransaction();
+                        SqlCommand mycommand2 = new SqlCommand("Insert into Shippers values('MyShop','8729298372')", conn);
+                        j = mycommand2.ExecuteNonQuery();
+                        Console.WriteLine("Inserted record in Shipper table :{0}",j);
+                        var mycommand = new SqlCommand("Insert into Region  values(9,'southwest')", conn);
+
+                        i = mycommand.ExecuteNonQuery();
+                        //Commiting transaction
+                        mytran.Commit();
+                      //  myscope.Complete();
+
+                    }
+                    catch (Exception e)
+                    {
+                        if(mytran!=null)
+                        {
+                            mytran.Rollback();
+                            Console.WriteLine(e);
+                            Console.WriteLine("Not Complete");
+
+                        }
+                       
+                    }
+
+
+                }
+
+
+            }
+
+        }
+
+
+        static void DistributedTransactionScope()
+        {
+            string myconnectionString = "Data Source = DESKTOP-U8J1M3C\\MSSQLSERVER01; Initial Catalog = Northwind; Integrated Security = true";
+            string myconnectionString1 = "Data Source = DESKTOP-U8J1M3C\\MSSQLSERVER01; Initial Catalog = dbEmployee873; Integrated Security = true";
+
+            using (TransactionScope myscope = new TransactionScope())
+            {
+                using (var myconn = new SqlConnection(myconnectionString))
+                {
+                    myconn.Open();
+                    var mycommand2 = new SqlCommand("Insert into Shippers values('VegatableShop','8729298372')", myconn);
+                    mycommand2.ExecuteNonQuery();
+                    using (var myconn1 = new
+                       SqlConnection(myconnectionString1))
+                    {
+                        myconn1.Open();
+                        var cmd = new SqlCommand("Insert into Book values('B005','AAAA')", myconn1);
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+                myscope.Complete();
+            }
+        }
+
+
+        static void Main()
+        {
+            try
+            {
+                LocalTransactionScope();
+               // DistributedTransactionScope();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            
+            Console.Read();
+
+        }
+
+    }
+}
