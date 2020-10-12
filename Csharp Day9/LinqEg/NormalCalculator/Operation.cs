709c2aaf6ba5be3584a60cc72c17584e26f17e01
monishanandhakumar <mmonishadevi.cse@gmail.com>
2020-10-12 18:16:47 +0530

    CSharp Day 9

---
+++ Csharp Day9/LinqEg/NormalCalculator/Operation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NormalCalculator
+{
+    public class Operation
+    {
+        public void Divide(int a,int b)
+
+        {
+
+            try
+            {
+                Console.WriteLine("Divide:{0}",(a/b));
+            }
+
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+        }
+    }
+}
---
+++ Csharp Day9/LinqEg/TypesOfClasses/SealedEg.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypesOfClasses
+{
+
+    class Rules
+    {
+
+    }
+  sealed  class Banking:Rules
+    {
+        string Revenu;
+
+      internal  void Display()
+        {
+            Console.WriteLine("Banking Personal information");
+        }
+    }
+    /*
+    class SealedEg:Banking
+    {
+    }*/
+    class MainApp
+    {
+        static void Main()
+        {
+            Banking b = new Banking();
+            b.Display();
+            Console.WriteLine();
+        }
+    }
+}
